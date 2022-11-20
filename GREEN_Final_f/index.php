5c383272436980cdeed2264e26da6b813a311872
AmigoTatar <124tbtm@gmail.com>
2022-11-20 07:00:51 +0500

    layout

---
+++ GREEN_Final_f/index.php
@@ -0,0 +1,202 @@
+<?php
+require_once("header.php") ;
+?>
+<!---////////////////////////////////////////////////////////////////////////-->
+
+  <div class="services" id="about">  
+            
+          <p> Салон оказывает парикмахерские услуги: </p><br>
+           <p> Стрижки: </p>
+     <p> мужские, женские, детские.</p> <br>
+
+           <p> Ногтевой сервис:</p> <br>
+    <p>  Маникюр, все виды, педикюр, аппаратный и классический, укрепление слабых ногтей. твёрдым материалом, донаращивание, наращивание(гель, полигель),выравнивание.
+      А также вы можете бесплатно проконсультироваться с мастером, по вопросам
+      cвязанным с уходом и состоянием ваших ноготков.Стаж работы мастера, более 10 лет.</p><br>
+
+      <p> Мастер бровист:</p><br>
+   <p> Моделирование и окрашивание бровей(краситель Австрия, хна),окрашивание ресниц( краситель Австрия). </p><br>
+
+      <p>  Визаж: </p><br>
+     <p>Макияж повседневный, деловой, вечерний, свадебный, детский. Наращивание ресниц: Классика, 2D, 3D</p><br>
+    </div>
+    
+	<div class="slider"> 
+
+ <div class="con-from-slide">
+
+  <div class="swiper-container main-slider loading">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide">
+    
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/1.jpg)">
+        <img src="pictute_slider/1.jpg" class="entity-img" />
+      </figure>
+
+      <div class="content">
+        <p class="title"></p>
+        <span class="caption"></span> <!--------------text-area--->
+      </div>
+    </div>
+    <div class="swiper-slide">
+     
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/2.jpg)">
+        <img src="pictute_slider/2.jpg" class="entity-img" />
+      </figure>
+
+      <div class="content">
+        <p class="title"></p>
+        <span class="caption"></span> <!--------------text-area--->
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/3.jpg)">
+        <img src="pictute_slider/3.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+        <span class="caption"></span> <!--------------text-area--->
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/4.jpg)">
+        <img src="pictute_slider/4.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+        <span class="caption"></span> <!--------------text-area--->
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/5.jpg)">
+        <img src="pictute_slider/5.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+        <span class="caption"></span> <!--------------text-area--->
+      </div>
+    </div>
+  </div>
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev swiper-button-white"></div>
+  <div class="swiper-button-next swiper-button-white"></div>
+</div>
+
+<!-- Thumbnail navigation -->
+<div class="swiper-container nav-slider loading">
+  <div class="swiper-wrapper">
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/1.jpg)">
+        <img src="pictute_slider/1.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/2.jpg)">
+        <img src="pictute_slider/2.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/3.jpg)">
+        <img src="pictute_slider/3.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/4.jpg)">
+        <img src="pictute_slider/4.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(pictute_slider/5.jpg)">
+        <img src="pictute_slider/5.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title"></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+  <!---------------------------------------------------------------------------->  
+
+
+
+
+  <!-----------------------------------scroll2------------------>
+
+<div class="scrol">
+          <a href="#section05"><span></span></a>
+        </section>
+        <section id="section05" class="demo">
+          <h1></h1>
+          <a href="#section06"><span></span></a>
+        </section>
+  </div>
+
+ <!-----------------------------------scroll2------------------>
+     <div class="tex-portfolo">
+
+  		<p>Сервис-Услуги</p>  		
+  	</div>
+
+  <div class="portfolio">
+
+  		<div class="portfol1" style="background-image: url(img/works/finished/item1-2.png)">
+  			<p class="portfolio_top_text">Маникюр</p>
+        <ul>
+          <li> Классический</li>
+          <li> Аппаратный</li>
+          <li> Европейский</li>
+          <li> Японский</li>
+      </ul>
+  		</div>
+  		<div class="portfol2" style="background-image: url(img/works/finished/item2-2.png)">
+  			
+  			<p class="portfolio_top_text">Стрижка</p>
+          <ul>
+          <li> Мужские.</li>
+          <li> Женские.</li>
+          <li> Детские</li>
+          <li></li>
+      </ul>
+  		</div>
+  		<div class="portfol3" style="background-image: url(img/works/finished/item3-2.png)">
+  			
+  			<p  class="portfolio_top_text">Окрашивание</p>
+          <ul>
+          <li> Меллирование</li>
+          <li> Колорирование</li>
+          <li> Тонирование</li>
+          <li>  прочее...</li>
+      </ul>
+  		</div>
+  		<div class="portfol4" style="background-image: url(img/works/finished/item4-2.png)">
+  			
+  			<p  class="portfolio_top_text">Визаж</p>
+          <ul>
+          <li> Деловой макияж</li>
+          <li> Дневной макияж</li>
+          <li> Вечерний макияж</li>
+          <li> прочее...</li>
+      </ul>
+  		</div>		
+</div>
+
+
+<!--////////////////////////////////////////////////////////////////////////////-->
+<?php
+require_once("footer.php") ;
+?>
---
+++ GREEN_Final_f/portfolio.php
@@ -0,0 +1,42 @@
+<?php
+require_once("header.php");
+
+?>
+<!--//////////////////////////////////////////-->
+<h2 class="hedSert">Портфолио</h2>
+
+<div class="gallerey">
+	<a href="gallerey/portfolio1.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio1_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio2.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio2_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio3.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio3_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio4.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio4_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio5.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio5_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio6.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio6_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio7.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio7_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio8.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio8_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio9.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio9_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio10.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio10_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio11.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio11_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio12.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio12_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio13.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio13_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio14.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio14_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio15.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio15_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio16.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio16_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio17.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio17_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio18.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio18_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio19.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio19_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio20.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio20_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio21.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio21_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio22.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio22_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio23.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio23_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio24.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio24_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio25.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio25_min.jpg" alt=""></a>
+	<a href="gallerey/portfolio26.jpg" data-lightbox="mygallery"><img src="gallerey/portfolio26_min.jpg" alt=""></a>
+	
+	
+</div>
+
+<!--//////////////////////////////////////////-->
+<?php 
+require_once("footer.php");
+?>
